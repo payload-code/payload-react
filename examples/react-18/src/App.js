124ff90cf5b579f99050ea58ed8895a5926aaaed
Jeff C <me@jeffca.me>
2023-07-28 13:51:09 -0500

    Dynamic Payload.js import, testing, linting, and examples (#13)
    * Create function to import Payload.js dynamically
    * Integration test written
    * Enhance react file to use the new function in our methods
    * Adding husky and eslint to maintain consistency in the code
    * Add examples to test package locally
    
    Signed-off-by: Jeff C <jeff@payload.co>

---
+++ examples/react-18/src/App.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+import PayloadReact from 'payload-react'
+
+function App() {
+  const formRef = useRef()
+  return (
+    <div className="App">
+      {/* eslint-disable-next-line */}
+      {/* eslint-disable-next-line */}
+      <PayloadReact.form.payment
+        client_token=""
+        className="container"
+        ref={formRef}
+      >
+        <PayloadReact.input.amount type="hidden" value="10.00" />
+        <div className="row pt-2">
+          <div className="form-group col7 px-1">
+            <label>Card Number</label>
+            {/* eslint-disable-next-line */}
+            <PayloadReact.input.card className="form-control" />
+          </div>
+        </div>
+        <div className="row pt-2">
+          <button className="btn btn-primary" type="submit">
+            Pay Now
+          </button>
+        </div>
+      </PayloadReact.form.payment>
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export async function getPayload() {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement('script')
+
+    s.setAttribute('src', 'https://payload.co/Payload.js')
+    s.addEventListener('load', resolve)
+    s.addEventListener('error', reject)
+
+    document.body.appendChild(s)
+  })
+}
