7aa7630f091acd72e1b35adcef94770eee4ab057
Ian Halpern <ian@payload.co>
2023-08-24 15:30:21 -0400

    build fixes

--- src/payload-react.js
+++ src/payload-react.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { getPayload } from './utils'
+import { getPayload } from './utils.js'
 
 const PayloadFormContext = React.createContext(null)
 
